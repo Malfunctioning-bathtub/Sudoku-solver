9e1c2a7001fe6cd0de604f801e1621466ac6abf3
Malfunctioning bathtub <jacobheller638@gmail.com>
2024-09-27 22:50:09 +0100

    Improved validation
    Added box validation and improved vertical and horizontal validation

--- main.py
+++ main.py
@@ -5,25 +5,44 @@ class Soduku():
         self.board = board
     def validate(self):
         for row in self.board:
-            usedNumbers = []
-            for cell in row:
-                cell = abs(cell)
-                if cell in usedNumbers:
+            if len(set(abs(i) for i in row if i != 0)) != len([abs(i) for i in row if i != 0]):
+                return False
+        
+        for column in zip(*self.board):
+            if len(set(abs(i) for i in column if i != 0)) != len([abs(i) for i in column if i != 0]):
+                return False
+        
+        for xbox in range(3):
+            for ybox in range(3):
+                subbox = [i[xbox*3:(xbox+1)*3] for i in self.board[ybox*3:(ybox+1)*3]]
+                values = [abs(i) for i in sum(subbox, []) if i != 0]
+                if len(set(values)) != len(values):
                     return False
-                if cell != 0:
-                    usedNumbers.append(cell)
         
-        for row in zip(*self.board):
-            usedNumbers = []
-            for cell in row:
-                cell = abs(cell)
-                if cell in usedNumbers:
-                    return False
-                if cell != 0:
-                    usedNumbers.append(cell)
-        
-        
+        return True
+    
+                
+                
+                
 
 
-test = Soduku(boards["invalid"])
+test = Soduku(boards["easiest"])
 print(test.validate())
+
+
+l1 = [1,2,3,4]
+l2 = [5,6,7,8]
+
+l2 += l1[1:3]
+
+[1,2,3, 4,5,6, 7,8,9] 
+[4,5,6, 7,8,9, 1,2,3]
+[7,8,9, 1,2,3, 4,5,6]
+
+[2,3,1, 5,6,4, 8,9,7]
+[5,6,4, 8,9,7, 2,3,1]
+[8,9,7, 2,3,1, 5,6,4]
+
+[3,1,2, 6,4,5, 9,7,8],
+[6,4,5, 9,7,8, 3,1,2],
+[9,7,8, 3,1,2, 6,4,5]
